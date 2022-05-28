9ffe1e1655258c7a739f43e8b0c3ea9f9ea6c058
audhalbritter <me@audhalbritter.com>
2022-05-28 08:40:08 +0200

    fix transformation of climate and vegetation data

--- R/tranformation_plan.R
+++ R/tranformation_plan.R
@@ -6,40 +6,129 @@ tranformation_plan <- list(
   # climate data
   tar_target(
     name = daily_temp,
-    command = climate %>%
-      tidylog::filter(!is.na(air_temperature),
-                      # only controls
-                      Nlevel %in% c(1, 2, 3),
-                      grazing == "C") %>%
-      mutate(date = dmy(format(date_time, "%d.%b.%Y"))) %>%
-      group_by(date, destSiteID, warming, turfID) %>%
-      summarise(air_temperature = mean(air_temperature)) %>%
-      # summer temp
-      mutate(month = month(date)) %>%
-      filter(month %in% c(5, 6, 7, 8)) %>%
-      group_by(destSiteID, warming) %>%
-      summarise(mean = mean(air_temperature),
-                se = sd(air_temperature)/sqrt(n()))
+    command = climate_raw %>%
+      # daily temperature
+      mutate(date = dmy(format(date_time, "%d.%b.%Y")),
+             warming = recode(warming, A = "Ambient", W = "Warming")) %>%
+      group_by(date, destSiteID, destBlockID, destPlotID, origSiteID, origBlockID, warming, Nlevel, grazing) %>%
+      summarise(air_temperature = mean(air_temperature, na.rm = TRUE),
+                ground_temperature = mean(ground_temperature, na.rm = TRUE),
+                soil_temperature = mean(soil_temperature, na.rm = TRUE),
+                soilmoisture = mean(soilmoisture, na.rm = TRUE)) |>
+      pivot_longer(cols = c(air_temperature:soilmoisture), names_to = "variable", values_to = "value") |>
+      filter(!is.na(value))
   ),
 
-  # cover
+  ## gridded climate data
+  # monthly
   tar_target(
-    name = cover_data,
+    name = monthly_climate,
+    command = gridded_climate_raw %>%
+      filter(variable %in% c("temperature", "precipitation")) |>
+      # monthly data
+      mutate(year = year(date),
+             date_month = dmy(paste0("15-",format(date, "%b.%Y")))) %>%
+      group_by(year, date_month, variable, siteID) %>%
+      summarise(sum = sum(value),
+                value = mean(value)) |>
+      mutate(value = ifelse(variable == "precipitation", sum, value)) %>%
+      select(-sum)
+
+  ),
+
+  # annual precipitation and summer temperature
+  tar_target(
+    name = annual_climate,
+    command = monthly_climate %>%
+      mutate(month = month(date_month)) |>
+      filter(variable == "temperature" & month %in% 6:9 | variable == "precipitation") |>
+      group_by(year, siteID, variable) %>%
+      summarise(se = sd(value)/sqrt(n()),
+                sum = sum(value),
+                value = mean(value)) |>
+      mutate(value = ifelse(variable == "precipitation", sum, value)) %>%
+      select(-sum)
+
+  ),
+
+
+  # biomass
+  tar_target(
+    name = biomass,
+    command = {
+      biomass_raw %>%
+        # calculate area in m2 and scale biomass to m2
+        mutate(area_m2 = area_cm2 / 10000,
+               biomass_scaled = biomass / area_m2
+        ) %>%
+        filter(year == 2021) %>%
+
+        # log transform Nitrogen
+        mutate(Nitrogen_log = log(Namount_kg_ha_y + 1)) |>
+
+        # summarise the cuts to get annual biomass/productivity
+        group_by(origSiteID, origBlockID, origPlotID, turfID, destSiteID, destBlockID, destPlotID, warming, Nlevel, Namount_kg_ha_y, Nitrogen_log, grazing, fun_group) %>%
+        summarise(productivity = sum(biomass_scaled)) %>%
+
+        # Calculate mean of 0 kg N per m2 y
+        ungroup() |>
+        group_by(origSiteID, origBlockID, origPlotID, turfID, destSiteID, destBlockID, destPlotID, warming, Namount_kg_ha_y, Nitrogen_log, grazing, fun_group) |>
+        tidylog::summarise(productivity = mean(productivity)) |>
+
+        mutate(origSiteID = recode(origSiteID, "Lia" = "Alpine", "Joa" = "Sub-alpine"),
+               origSiteID = factor(origSiteID, levels = c("Alpine", "Sub-alpine")),
+               grazing = factor(grazing, levels = c("C", "M", "I", "N")),
+               grazing = recode(grazing, "C" = "Control", "M" = "Medium", "I" = "Intensive", "N" = "Natural"),
+               warming = recode(warming, "A" = "Ambient", "W" = "Warming"))
+
+    }),
+
+
+  # prep cover
+  tar_target(
+    name = cover,
+    command = cover_raw %>%
+      # first and last year
+      filter(year %in% c(2019, 2021),
+             # just for now!!!
+             !str_detect(species, "Carex")) %>%
+
+      # prettify
+      mutate(origSiteID = recode(origSiteID, "Lia" = "Alpine", "Joa" = "Sub-alpine"),
+             origSiteID = factor(origSiteID, levels = c("Alpine", "Sub-alpine")),
+             grazing = factor(grazing, levels = c("C", "M", "I", "N")),
+             grazing = recode(grazing, "C" = "Control", "M" = "Medium", "I" = "Intensive", "N" = "Natural"),
+             warming = recode(warming, "A" = "Ambient", "W" = "Warming")) %>%
+
+      # add Namount variable
+      left_join(metaTurfID %>%
+                  distinct(Nlevel, Namount_kg_ha_y), by = "Nlevel") %>%
+      # log transform Nitrogen
+      mutate(Nitrogen_log = log(Namount_kg_ha_y + 1)) |>
+
+      # take average cover of 3 control plots
+      group_by(year, date, origSiteID, origBlockID, origPlotID, destSiteID, destBlockID, destPlotID, turfID, warming, grazing, Namount_kg_ha_y, Nitrogen_log, species) |>
+      summarise(cover = mean(cover)) |>
+
+      # add taxon information
+      left_join(sp_list, by = "species")
+  ),
+
+  # Change in functional group cover
+  tar_target(
+    name = functional_group_cover,
     command = cover %>%
-      left_join(sp_list, by = "species") %>%
       group_by(turfID, origBlockID, origSiteID, warming, grazing, Namount_kg_ha_y, Nitrogen_log, functional_group, year) %>%
       summarise(cover = sum(cover)) %>%
       pivot_wider(names_from = year, values_from = cover) %>%
       mutate(delta = `2021` - `2019`) %>%
-      #filter(functional_group %in% c("forb", "graminoid")) %>%
       ungroup()
   ),
 
   # richness
   tar_target(
-    name = diversity_data,
+    name = diversity,
     command = cover %>%
-      ungroup() %>%
       group_by(turfID, origBlockID, origSiteID, year, warming, grazing, Namount_kg_ha_y, Nitrogen_log) %>%
       summarise(richness = n(),
                 diversity = diversity(cover),
@@ -48,25 +137,6 @@ tranformation_plan <- list(
       pivot_wider(names_from = year, values_from = value) %>%
       mutate(delta = `2021` - `2019`) %>%
       ungroup()
-  ),
-
-  # biomass
-  tar_target(
-    name = biomass_data,
-    command = {
-      biomass_raw %>%
-        # scale biomass to 50 x 50 cm plot
-        mutate(biomass_scaled = if_else(area_cm2 == 2500, biomass, biomass * 2500 / area_cm2)) %>%
-        filter(year == 2021) %>%
-        # summarise to get annual biomass/productivity
-        group_by(origSiteID, origBlockID, origPlotID, turfID, destSiteID, destBlockID, destPlotID, warming, Nlevel, Namount_kg_ha_y, grazing, fun_group) %>%
-        summarise(productivity = sum(biomass_scaled)) %>%
-        mutate(origSiteID = recode(origSiteID, "Lia" = "High alpine", "Joa" = "Alpine"),
-               origSiteID = factor(origSiteID, levels = c("High alpine", "Alpine")),
-               grazing = factor(grazing, levels = c("C", "M", "I", "N")),
-               grazing = recode(grazing, "C" = "Control", "M" = "Medium", "I" = "Intensive", "N" = "Natural"),
-               warming = recode(warming, "A" = "ambient", "W" = "warming"))
-
-    })
+  )
 
 )
