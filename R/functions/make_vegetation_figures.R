d2f3bc88a56fee1e21987e976a20956da70d696c
audhalbritter <me@audhalbritter.com>
2022-06-02 12:10:56 +0200

    make veg figures

--- R/functions/make_vegetation_figures.R
+++ R/functions/make_vegetation_figures.R
@@ -45,3 +45,62 @@ make_productivity_figure <- function(biomass){
 
 }
 
+# 3 colour palette (grey, orange, pink)
+#three_col_palette <- c("#999999", "#E69F00", "#CC79A7")
+
+#        sign = if_else(is.na(sign),  "no effect", sign),
+#        sign = recode(sign, W_sign = "Warming", NxW_sign = "Interaction"),
+#        sign = factor(sign, levels = c("no effect", "Warming", "Interaction"))) |>
+
+make_cover_fig <- function(subset, fig_title, colour_palette, facet_1) {
+
+  p <- subset |>
+    ggplot(aes(x = Nitrogen_log, y = delta, colour = sign, linetype = warming, shape = warming)) +
+    geom_smooth(method = "lm", se = TRUE, aes(fill = sign)) +
+    geom_point(size = 2) +
+    labs(x = bquote(log(Nitrogen)~(kg~ha^-1~y^-1))) +
+    scale_colour_manual(name = "Effect", values = colour_palette) +
+    scale_fill_manual(name = "Effect", values = colour_palette) +
+    scale_linetype_manual(name = "Warming", values = c("dashed", "solid")) +
+    scale_shape_manual(name = "Warming", values = c(1, 16)) +
+    theme_minimal() +
+    theme(legend.position = "top",
+          text = element_text(size = 16))
+
+  if(facet_1 == "functional_group"){
+    cover_figure <- p +
+      labs(y = "Change in cover",
+           title = fig_title) +
+      facet_grid(functional_group ~ grazing)
+
+  } else
+    {
+
+      cover_figure <- p +
+        labs(y = "Change in richness",
+             title = fig_title) +
+        facet_grid(origSiteID ~ grazing)
+
+  }
+
+  return(cover_figure)
+
+}
+
+
+
+# first panel for presentations
+# panel1 <- cover_data %>%
+#   filter(functional_group == "graminoid",
+#          grazing =="Control",
+#          origSiteID == "High alpine") %>%
+#   ggplot(aes(x = Nitrogen_log, y = delta, colour = functional_group, linetype = warming, shape = warming)) +
+#   geom_point() +
+#   geom_smooth(method = "lm", se = FALSE) +
+#   labs(x = bquote(log(Nitrogen)~(kg~ha^-1~y^-1)),
+#        y = "Difference in cover (2021 - 2019)") +
+#   scale_colour_manual(name = "", values = c("limegreen")) +
+#   scale_linetype_manual(name = "warming", values = c("solid", "dashed")) +
+#   scale_shape_manual(name = "warming", values = c(16, 1)) +
+#   lims(y = c(-60, 80)) +
+#   theme_minimal()
