6099f9cb495df2016db0d8985c72791ff0524e52
audhalbritter <me@audhalbritter.com>
2022-06-02 12:11:25 +0200

    add functions and plans

--- _targets.R
+++ _targets.R
@@ -1,24 +1,28 @@
 library("targets")
 library("tarchetypes")
 
-tar_option_set(packages = c("rjt.misc", "sf"))
+tar_option_set(packages = c("sf", "dataDownloader", "tidyverse", "readxl", "vegan", "viridis", "lubridate", "MuMIn", "performance", "broom"))
+#"rjt.misc",
 
 # source other scripts
-source("R/load_libraries.R")
+#source("R/load_libraries.R")
+source("R/functions/make_climate_figures.R")
+source("R/functions/make_vegetation_figures.R")
+source("R/functions/make_analysis.R")
 
 # source target plans - can also construct plans directly in this file.
 source("R/download_plan.R")
 source("R/tranformation_plan.R")
-# source("R/analysis_plan.R")
+source("R/analysis_plan.R")
 source("R/figure_plan.R")
-source("R/manuscript_plan.R")
+# source("R/manuscript_plan.R")
 
 
 #Combine target plans
 combined_plan <- c(
   download_plan,
   tranformation_plan,
-  # analysis_plan,
-  figure_plan,
-  manuscript_plan
+  analysis_plan,
+  figure_plan
+  # manuscript_plan
 )
