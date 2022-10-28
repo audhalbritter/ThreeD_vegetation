8544b54c214ff0563f7ca5ee7105ef965a2bd58b
audhalbritter <me@audhalbritter.com>
2022-10-28 11:18:19 +0200

    add extra year to biomass data

--- R/tranformation_plan.R
+++ R/tranformation_plan.R
@@ -10,7 +10,7 @@ tranformation_plan <- list(
       # daily temperature
       mutate(date = dmy(format(date_time, "%d.%b.%Y")),
              warming = recode(warming, A = "Ambient", W = "Warming")) %>%
-      group_by(date, destSiteID, destBlockID, destPlotID, origSiteID, origBlockID, warming, Nlevel, grazing) %>%
+      group_by(date, destSiteID, destBlockID, destPlotID, origSiteID, origBlockID, warming, Nlevel, grazing, Namount_kg_ha_y) %>%
       summarise(air_temperature = mean(air_temperature, na.rm = TRUE),
                 ground_temperature = mean(ground_temperature, na.rm = TRUE),
                 soil_temperature = mean(soil_temperature, na.rm = TRUE),
@@ -61,18 +61,18 @@ tranformation_plan <- list(
         mutate(area_m2 = area_cm2 / 10000,
                biomass_scaled = biomass / area_m2
         ) %>%
-        filter(year == 2021) %>%
+        filter(year %in% c(2021, 2022)) %>%
 
         # log transform Nitrogen
         mutate(Nitrogen_log = log(Namount_kg_ha_y + 1)) |>
 
         # summarise the cuts to get annual biomass/productivity
-        group_by(origSiteID, origBlockID, origPlotID, turfID, destSiteID, destBlockID, destPlotID, warming, Nlevel, Namount_kg_ha_y, Nitrogen_log, grazing, fun_group) %>%
+        group_by(origSiteID, origBlockID, origPlotID, turfID, destSiteID, destBlockID, destPlotID, warming, Nlevel, Namount_kg_ha_y, Nitrogen_log, grazing, fun_group, year) %>%
         summarise(productivity = sum(biomass_scaled)) %>%
 
         # Calculate mean of 0 kg N per m2 y
         ungroup() |>
-        group_by(origSiteID, origBlockID, origPlotID, turfID, destSiteID, destBlockID, destPlotID, warming, Namount_kg_ha_y, Nitrogen_log, grazing, fun_group) |>
+        group_by(origSiteID, origBlockID, origPlotID, turfID, destSiteID, destBlockID, destPlotID, warming, Namount_kg_ha_y, Nitrogen_log, grazing, fun_group, year) |>
         tidylog::summarise(productivity = mean(productivity)) |>
 
         mutate(origSiteID = recode(origSiteID, "Lia" = "Alpine", "Joa" = "Sub-alpine"),
@@ -89,7 +89,7 @@ tranformation_plan <- list(
     name = cover,
     command = cover_raw %>%
       # first and last year
-      filter(year %in% c(2019, 2021),
+      filter(year %in% c(2019, 2022),
              # just for now!!!
              !str_detect(species, "Carex")) %>%
 
@@ -107,7 +107,7 @@ tranformation_plan <- list(
       mutate(Nitrogen_log = log(Namount_kg_ha_y + 1)) |>
 
       # take average cover of 3 control plots
-      group_by(year, date, origSiteID, origBlockID, origPlotID, destSiteID, destBlockID, destPlotID, turfID, warming, grazing, Namount_kg_ha_y, Nitrogen_log, species) |>
+      group_by(year, date, turfID, origSiteID, origBlockID, destSiteID, destBlockID, warming, grazing, Namount_kg_ha_y, Nitrogen_log, species) |>
       summarise(cover = mean(cover)) |>
 
       # add taxon information
@@ -124,8 +124,8 @@ tranformation_plan <- list(
       # Fun groups that do not exist in one year => 0
       # calculate difference between years
       mutate(`2019` = if_else(is.na(`2019`), 0, `2019`),
-             `2021` = if_else(is.na(`2021`), 0, `2021`),
-             delta = `2021` - `2019`) %>%
+             `2022` = if_else(is.na(`2022`), 0, `2022`),
+             delta = `2022` - `2019`) %>%
       ungroup()
   ),
 
@@ -144,7 +144,7 @@ tranformation_plan <- list(
       group_by(origSiteID, year, warming, grazing, Namount_kg_ha_y, Nitrogen_log, diversity_index) |>
       summarise(value = mean(value)) |>
       pivot_wider(names_from = year, values_from = value) %>%
-      mutate(delta = `2021` - `2019`) %>%
+      mutate(delta = `2022` - `2019`) %>%
       ungroup()
   )
 
