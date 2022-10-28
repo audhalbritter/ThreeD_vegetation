694305fb4eca80a573262125f9438f8c08bd9b18
audhalbritter <me@audhalbritter.com>
2022-10-28 10:11:08 +0200

    add figure on climate treatments

--- R/functions/make_climate_figures.R
+++ R/functions/make_climate_figures.R
@@ -38,3 +38,107 @@ make_daily_climate_figure <- function(daily_temp){
 }
 
 
+
+# climate treatment figure
+make_climate_treatment_figure <- function(daily_temp){
+
+  df <- daily_temp |>
+    mutate(month = month(date),
+           year = year(date)) |>
+    filter(month %in% c(5, 6, 7, 8, 9),
+           grazing == "C",
+           Namount_kg_ha_y == 0)
+    warm <- ggplot(df, aes(x = warming, y = value, fill = warming)) +
+    geom_violin(draw_quantiles = 0.5) +
+    annotate("text", x = Inf, y =Inf, label = "*", hjust = 1, vjust = 1, size = 10, colour = "#CC79A7") +
+    scale_fill_manual(values = c("grey", "#CC79A7")) +
+    labs(x = "") +
+    facet_wrap(~ variable, scales = "free_y", nrow = 1) +
+    theme_bw() +
+    theme(legend.position = "none")
+
+  grazing <- daily_temp |>
+    mutate(month = month(date),
+           year = year(date)) |>
+    filter(month %in% c(5, 6, 7, 8, 9),
+           warming == "Ambient",
+           Namount_kg_ha_y == 0) |>
+    mutate(grazing = recode(grazing, C = "Control", M = "Medium", I = "Intensive"),
+           grazing = factor(grazing, levels = c("Control", "Medium", "Intensive"))) |>
+    ggplot(aes(x = grazing, y = value, fill = grazing)) +
+    geom_violin(draw_quantiles = 0.5) +
+    scale_fill_manual(values = c("grey", "coral", "coral4")) +
+    labs(x = "") +
+    facet_wrap(~ variable, scales = "free_y", nrow = 1) +
+    theme_bw() +
+    theme(legend.position = "none")
+
+
+  nitrogen <- daily_temp |>
+    mutate(month = month(date),
+           year = year(date)) |>
+    filter(month %in% c(5, 6, 7, 8, 9),
+           warming == "Ambient",
+           grazing == "C") |>
+    ggplot(aes(x = factor(Namount_kg_ha_y), y = value, fill = factor(Namount_kg_ha_y))) +
+    geom_violin(draw_quantiles = 0.5) +
+    scale_fill_viridis_d(option = "inferno", direction = -1) +
+    labs(x = "") +
+    facet_wrap(~ variable, scales = "free_y", nrow = 1) +
+    theme_bw() +
+    theme(legend.position = "none")
+
+  climate_treatment <- warm / grazing / nitrogen
+
+  return(climate_treatment)
+}
+
+
+
+
+# daily_temp |>
+#   mutate(month = month(date),
+#          year = year(date)) |>
+#   filter(month %in% c(5, 6, 7, 8, 9),
+#          grazing == "C",
+#          Namount_kg_ha_y == 0) |>
+#   group_by(variable) |>
+#   nest(data = -variable) %>%
+#   mutate(fit = map(data, ~ lm(value ~ warming, data = .x)),
+#          fit_tidy = map(fit, tidy)) |>
+#   unnest(fit_tidy)
+#
+# dd <- daily_temp |>
+#   mutate(month = month(date),
+#          year = year(date)) |>
+#   filter(month %in% c(5, 6, 7, 8, 9),
+#          grazing == "C",
+#          Namount_kg_ha_y == 0,
+#          variable == "soilmoisture")
+# fit <- lm(value ~ warming * origSiteID, data = dd)
+# check_model(fit)
+#
+#
+# daily_temp |>
+#   mutate(month = month(date),
+#          year = year(date)) |>
+#   filter(month %in% c(5, 6, 7, 8, 9),
+#          warming == "Ambient",
+#          Namount_kg_ha_y == 0) |>
+#   group_by(variable) |>
+#   nest(data = -variable) %>%
+#   mutate(fit = map(data, ~ lm(value ~ grazing, data = .x)),
+#          fit_tidy = map(fit, tidy)) |>
+#   unnest(fit_tidy)
+#
+# daily_temp |>
+#   mutate(month = month(date),
+#          year = year(date)) |>
+#   filter(month %in% c(5, 6, 7, 8, 9),
+#          warming == "Ambient",
+#          grazing == "C") |>
+#   group_by(variable) |>
+#   nest(data = -variable) %>%
+#   mutate(fit = map(data, ~ lm(value ~ Namount_kg_ha_y, data = .x)),
+#          fit_tidy = map(fit, tidy)) |>
+#   unnest(fit_tidy)
