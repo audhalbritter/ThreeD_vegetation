e2cd8fde8c3d2fff9cae2e3d1692cdfe66a328aa
audhalbritter <me@audhalbritter.com>
2023-05-18 10:50:56 +0200

    add bibliography

--- R/manuscript_plan.R
+++ R/manuscript_plan.R
@@ -1,26 +1,26 @@
 #
 manuscript_plan <- list(
 
-  # bibliography
-  tar_target(
-    name = bibliography,
-    command = "Manuscript/bibliography.bib",
-    format = "file"
-  ),
+  # # bibliography
+  # tar_target(
+  #   name = bibliography,
+  #   command = "Manuscript/bibliography.bib",
+  #   format = "file"
+  # ),
+  #
+  # # add packages to bibliography
+  # tar_target(
+  #   name = biblio2,
+  #   command = rjt.misc::package_citations(
+  #     packages = c("targets", "tidyverse", "rmarkdown"),
+  #     old_bib = bibliography,
+  #     new_bib = "Manuscript/bibliography2.bib"),
+  #   format = "file"
+  # ),
 
-  # add packages to bibliography
-  tar_target(
-    name = biblio2,
-    command = rjt.misc::package_citations(
-      packages = c("targets", "tidyverse", "rmarkdown"),
-      old_bib = bibliography,
-      new_bib = "Manuscript/bibliography2.bib"),
-    format = "file"
-  ),
-
-  #render manuscript
-  tar_render(name = ms,
-             path = "Manuscript/Manuscript.Rmd")
+  # Results
+  tar_render(name = results,
+             path = "Manuscript/Main.qmd")
 
 
 )
