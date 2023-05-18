b8b51cf512ab3ec3a9700c93e0d445bde45516db
audhalbritter <me@audhalbritter.com>
2023-05-18 10:52:14 +0200

    make climate figures

--- R/functions/make_climate_figures.R
+++ R/functions/make_climate_figures.R
@@ -3,15 +3,30 @@
 # annual climate
 make_annual_climate_figure <- function(annual_climate){
 
-  annual_climate_figure <- annual_climate |>
+  dat <- annual_climate |>
     pivot_wider(names_from = "variable", values_from = c(value, se)) |>
-    mutate(siteID = factor(siteID, levels = c("Vikesland", "Joasete", "Liahovden"))) |>
+    mutate(siteID = factor(siteID, levels = c("Vikesland", "Joasete", "Liahovden")))
+
+  means <- dat |>
+    group_by(siteID) |>
+    summarise(prec = mean(value_precipitation),
+              temp = mean(value_temperature))
+
+  annual_climate_figure <- dat |>
     ggplot(aes(x = value_precipitation, y = value_temperature, shape = siteID, colour = year)) +
     geom_point(size = 3) +
+    geom_errorbar(aes(ymin = value_temperature - se_temperature,
+                      ymax = value_temperature + se_temperature)) +
+    geom_errorbarh(aes(xmin = value_precipitation - se_precipitation,
+                       xmax = value_precipitation + se_precipitation)) +
+    geom_point(data = means, aes(x = prec, y = temp), colour = "#E69F00", size = 5) +
     scale_colour_viridis_c() +
     scale_shape_manual(values = c(15, 16, 17)) +
     labs(x = "Annual precipitation in mm", y = "Summer temperature in °C") +
-    theme_minimal()
+    theme_bw() +
+    theme(legend.position = "top",
+          legend.box ="vertical",
+          text = element_text(size = 17))
 
   return(annual_climate_figure)
 
@@ -21,99 +36,87 @@ make_annual_climate_figure <- function(annual_climate){
 # daily climate in control and warming
 make_daily_climate_figure <- function(daily_temp){
 
+  tar_load(col_palette)
+
   daily_climate <- daily_temp |>
     filter(Nlevel == 1,
-           grazing == "C") |>
-    mutate(origSiteID = recode(origSiteID, Joa = "Sub-alpine", Lia = "Alpine"),
-           variable = recode(variable, air_temperature = "air", ground_temperature = "ground", soil_temperature = "soil")) |>
+           grazing == "Control") |>
     ggplot(aes(x = date, y = value, colour = warming)) +
     geom_line() +
-    scale_color_manual(values = c("grey", "#CC79A7")) +
-    labs(x = "", y = "soilmoisture in %                           temperature in °C") +
+    scale_color_manual(values = col_palette) +
+    labs(x = "", y = "soilmoisture in %         temperature in °C",
+         tag = "a)") +
     facet_grid(variable ~ origSiteID, scales = "free_y") +
-    theme_minimal() +
+    theme_bw() +
     theme(legend.position = "top")
 
   summer_mean <- daily_temp |>
     mutate(month = month(date),
            year = year(date)) |>
     filter(month %in% c(5, 6, 7, 8, 9),
-           grazing == "C",
+           grazing == "Control",
            Namount_kg_ha_y == 0) |>
-    group_by(variable, warming, origSiteID) |>
-    summarise(se = sd(value)/sqrt(n()),
-              value = mean(value)) |>
-    mutate(origSiteID = recode(origSiteID, Joa = "Sub-alpine", Lia = "Alpine"),
-           variable = recode(variable, air_temperature = "air", ground_temperature = "ground", soil_temperature = "soil")) |>
-    ggplot(aes(x = warming, y = value, colour = warming)) +
-    geom_point(size = 2) +
-    geom_errorbar(aes(ymin = value - se, ymax = value + se), width = 0.2) +
-    scale_colour_manual(values = c("grey", "#CC79A7")) +
-    labs(x = "", y = "") +
+    # group_by(variable, warming, origSiteID) |>
+    # summarise(se = sd(value)/sqrt(n()),
+    #           value = mean(value)) |>
+    ggplot(aes(x = warming, y = value, fill = warming)) +
+    geom_violin(draw_quantiles = c(0.5)) +
+    # geom_point(size = 2, shape = 16) +
+    # geom_errorbar(aes(ymin = value - se, ymax = value + se), width = 0.2) +
+    scale_fill_manual(values = col_palette) +
+    annotate("text", x = Inf, y =Inf, label = "*", hjust = 1, vjust = 1, size = 10, colour = col_palette[2]) +
+    labs(x = "", y = "",
+         tag = "b)") +
     facet_grid(variable ~ origSiteID, scales = "free_y") +
-    theme_minimal() +
-    theme(legend.position = "none")
+    theme_bw() +
+    theme(axis.text.x = element_blank())
 
-  daily_climate_figure <- daily_climate + summer_mean + plot_layout(widths = c(3, 1))
+  daily_climate_figure <- daily_climate + summer_mean +
+    plot_layout(widths = c(3, 1.5), guides = "collect") &
+    theme(legend.position = "bottom")
 
-  return(daily_climate_figure)
 }
 
 
 
 # climate treatment figure
-# make_climate_treatment_figure <- function(daily_temp){
-#
-#   df <- daily_temp |>
-#     mutate(month = month(date),
-#            year = year(date)) |>
-#     filter(month %in% c(5, 6, 7, 8, 9),
-#            grazing == "C",
-#            Namount_kg_ha_y == 0)
-#     warm <- ggplot(df, aes(x = warming, y = value, fill = warming)) +
-#     geom_violin(draw_quantiles = 0.5) +
-#     annotate("text", x = Inf, y =Inf, label = "*", hjust = 1, vjust = 1, size = 10, colour = "#CC79A7") +
-#     scale_fill_manual(values = c("grey", "#CC79A7")) +
-#     labs(x = "") +
-#     facet_wrap(~ variable, scales = "free_y", nrow = 1) +
-#     theme_bw() +
-#     theme(legend.position = "none")
-#
-#   grazing <- daily_temp |>
-#     mutate(month = month(date),
-#            year = year(date)) |>
-#     filter(month %in% c(5, 6, 7, 8, 9),
-#            warming == "Ambient",
-#            Namount_kg_ha_y == 0) |>
-#     mutate(grazing = recode(grazing, C = "Control", M = "Medium", I = "Intensive"),
-#            grazing = factor(grazing, levels = c("Control", "Medium", "Intensive"))) |>
-#     ggplot(aes(x = grazing, y = value, fill = grazing)) +
-#     geom_violin(draw_quantiles = 0.5) +
-#     scale_fill_manual(values = c("grey", "coral", "coral4")) +
-#     labs(x = "") +
-#     facet_wrap(~ variable, scales = "free_y", nrow = 1) +
-#     theme_bw() +
-#     theme(legend.position = "none")
-#
-#
-#   nitrogen <- daily_temp |>
-#     mutate(month = month(date),
-#            year = year(date)) |>
-#     filter(month %in% c(5, 6, 7, 8, 9),
-#            warming == "Ambient",
-#            grazing == "C") |>
-#     ggplot(aes(x = factor(Namount_kg_ha_y), y = value, fill = factor(Namount_kg_ha_y))) +
-#     geom_violin(draw_quantiles = 0.5) +
-#     scale_fill_viridis_d(option = "inferno", direction = -1) +
-#     labs(x = "") +
-#     facet_wrap(~ variable, scales = "free_y", nrow = 1) +
-#     theme_bw() +
-#     theme(legend.position = "none")
-#
-#   climate_treatment <- warm / grazing / nitrogen
-#
-#   return(climate_treatment)
-# }
+make_climate_treatment_figure <- function(daily_temp){
+
+  grazing <- daily_temp |>
+    mutate(month = month(date),
+           year = year(date)) |>
+    filter(month %in% c(5, 6, 7, 8, 9),
+           warming == "Ambient",
+           Namount_kg_ha_y == 0) |>
+    ggplot(aes(x = grazing, y = value, fill = grazing)) +
+    geom_violin(draw_quantiles = 0.5) +
+    scale_fill_manual(values = c("grey", "coral", "coral4")) +
+    labs(x = "",
+         tag = "a)") +
+    facet_grid2(origSiteID ~ variable, scales = "free_y", independent = "y") +
+    theme_bw() +
+    theme(legend.position = "none")
+
+
+  nitrogen <- daily_temp |>
+    mutate(month = month(date),
+           year = year(date)) |>
+    filter(month %in% c(5, 6, 7, 8, 9),
+           warming == "Ambient",
+           grazing == "Control") |>
+    ggplot(aes(x = factor(Namount_kg_ha_y), y = value, fill = factor(Namount_kg_ha_y))) +
+    geom_violin(draw_quantiles = 0.5) +
+    scale_fill_viridis_d(option = "inferno", direction = -1) +
+    labs(x = bquote(Nitrogen~kg~ha^-1~y^-1),
+         tag = "b)") +
+    facet_grid2(origSiteID ~ variable, scales = "free_y", independent = "y") +
+    theme_bw() +
+    theme(legend.position = "none")
+
+  climate_treatment <- grazing / nitrogen
+
+  return(climate_treatment)
+}
 
 
 
