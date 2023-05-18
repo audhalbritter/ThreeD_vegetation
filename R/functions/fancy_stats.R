682b269c9bb45ece3ebb13c95ab95f958b626d85
audhalbritter <me@audhalbritter.com>
2023-05-18 10:49:33 +0200

    add fancy stats name function

---
+++ R/functions/fancy_stats.R
@@ -0,0 +1,36 @@
+# Fancy stats dictionary
+
+fancy_stats <- function(dat){
+
+  dat <- dat %>%
+    mutate(term = recode(term,
+                         "(Intercept)" = "Intercept",
+                         "warmingWarming" = "W",
+                         ".grazing" = "G",
+                         ".grazingNatural" = "G",
+                         "Namount_kg_ha_y" = "N",
+                         "poly(Namount_kg_ha_y, 2)1" = "N",
+                         "poly(Namount_kg_ha_y, 2)2" = "N^2",
+                         "warmingWarming:.grazing" = "WxG",
+                         "warmingWarming:.grazingNatural" = "WxG",
+                         "warmingWarming:Namount_kg_ha_y" = "WxN",
+                         "warmingWarming:poly(Namount_kg_ha_y, 2)1" = "WxN",
+                         "warmingWarming:poly(Namount_kg_ha_y, 2)2" = "WxN^2",
+                         ".grazing:Namount_kg_ha_y" = "GxN",
+                         ".grazingNatural:Namount_kg_ha_y" = "GxN",
+                         ".grazing:poly(Namount_kg_ha_y, 2)1" = "GxN",
+                         ".grazing:poly(Namount_kg_ha_y, 2)2" = "GxN^2",
+                         ".grazingNatural:poly(Namount_kg_ha_y, 2)1" = "GxN",
+                         ".grazingNatural:poly(Namount_kg_ha_y, 2)2" = "GxN^2",
+                         "warmingWarming:.grazing:Namount_kg_ha_y" = "WxGxN",
+                         "warmingWarming:.grazingNatural:Namount_kg_ha_y" = "WxGxN",
+                         "warmingWarming:.grazing:poly(Namount_kg_ha_y, 2)1" = "WxGxN",
+                         "warmingWarming:.grazing:poly(Namount_kg_ha_y, 2)2" = "WxGxN^2",
+                         "warmingWarming:.grazingNatural:poly(Namount_kg_ha_y, 2)1" = "WxGxN",
+                         "warmingWarming:.grazingNatural:poly(Namount_kg_ha_y, 2)2" = "WxGxN^2",
+                         "Nitrogen_log" = "N",
+                         "warmingWarming:Nitrogen_log" = "WxN",
+                         ".grazing:Nitrogen_log" = "GxN",
+                         "warmingWarming:.grazing:Nitrogen_log" = "WxGxN"))
+  return(dat)
+}
