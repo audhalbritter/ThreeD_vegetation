eed2cc2cf8f1f35778a1cec571f77f5be4b93a8e
audhalbritter <me@audhalbritter.com>
2023-06-02 13:54:04 +0200

    use tar_quarto

--- R/manuscript_plan.R
+++ R/manuscript_plan.R
@@ -1,26 +1,8 @@
 #
 manuscript_plan <- list(
 
-  # # bibliography
-  # tar_target(
-  #   name = bibliography,
-  #   command = "Manuscript/bibliography.bib",
-  #   format = "file"
-  # ),
-  #
-  # # add packages to bibliography
-  # tar_target(
-  #   name = biblio2,
-  #   command = rjt.misc::package_citations(
-  #     packages = c("targets", "tidyverse", "rmarkdown"),
-  #     old_bib = bibliography,
-  #     new_bib = "Manuscript/bibliography2.bib"),
-  #   format = "file"
-  # ),
-
-  # Results
-  tar_render(name = results,
-             path = "Manuscript/Main.qmd")
-
+  # manuscript
+  tar_quarto(name = manuscript,
+             path = "Main.qmd")
 
 )
