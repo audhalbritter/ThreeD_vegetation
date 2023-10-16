83292c44a3175673bc78892cc0d6aacfd5badcaa
audhalbritter <me@audhalbritter.com>
2023-10-16 11:02:29 +0200

    update text, figure and table captions

--- R/manuscript_plan.R
+++ R/manuscript_plan.R
@@ -3,6 +3,10 @@ manuscript_plan <- list(
 
   # manuscript
   tar_quarto(name = manuscript,
-             path = "Main.qmd")
+             path = "Main.qmd"),
+
+  # SI
+  tar_quarto(name = si,
+             path = "SI.qmd")
 
 )
