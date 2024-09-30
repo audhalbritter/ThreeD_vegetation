5e4ee7e144dc76a3e8c37a80af1d6141f312a9f5
audhalbritter <me@audhalbritter.com>
2024-09-30 09:18:05 +0200

    add sem plan

--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 library("targets")
 library("tarchetypes")
 
-tar_option_set(packages = c("sf", "dataDownloader", "tidyverse", "readxl", "vegan", "viridis", "lubridate", "MuMIn", "performance", "broom", "DBI", "RSQLite", "dataDocumentation", "patchwork", "wesanderson", "ggh4x", "gt", "ggpubr"))
+tar_option_set(packages = c("sf", "dataDownloader", "tidyverse", "readxl", "vegan", "viridis", "lubridate", "MuMIn", "performance", "broom", "DBI", "RSQLite", "dataDocumentation", "patchwork", "wesanderson", "ggh4x", "gt", "ggpubr", "lavaan", "tidySEM", "quarto"))
 # sf, DBI, RSQLite, MuMin?
 
 # source other scripts
@@ -13,6 +13,7 @@ combined_plan <- c(
   download_plan,
   tranformation_plan,
   analysis_plan,
+  sem_plan,
   figure_plan,
   #manuscript_plan,
   si_analysis_plan,
