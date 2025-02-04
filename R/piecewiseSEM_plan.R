32f793a6ff0d8f9c60170941ff7db120a503f31e
audhalbritter <me@audhalbritter.com>
2025-02-04 16:33:28 +0100

    add targets for new biomass and diversity sems

--- R/piecewiseSEM_plan.R
+++ R/piecewiseSEM_plan.R
@@ -2,12 +2,15 @@
 
 piecewiseSEM_plan <- list(
 
-  # SEM cutting and diversity
+  # SEM final year biomass and diversity
+  # cutting
   tar_target(
     name = prep_cutting_data,
     command = prep_SEM_data(data = biomass_div,
                             landuse = "cutting",
-                            diversity = log_ratio_diversity)
+                            diversity = final_diversity,
+                            biomass = final_bio,
+                            change = FALSE)
   ),
 
   # run sem for drivers, biomass (calculated) and diversity, only cutting
@@ -31,13 +34,57 @@ piecewiseSEM_plan <- list(
                               landuse = "cutting")
   ),
 
+  # only direct effects (log ratio)
+  tar_target(
+    name = direct_sem_cutting,
+    command = run_direct_SEM(data = prep_cutting_data,
+                             landuse = "cutting",
+                             diversity = log_ratio_diversity,
+                             biomass = log_ratio_bio)
+  ),
 
-  # SEM grazing and diversity
+  # SEM change in biomass and diversity
+  # cutting
+  tar_target(
+    name = prep_cutting_change_data,
+    command = prep_SEM_data(data = biomass_div,
+                            landuse = "cutting",
+                            diversity = log_ratio_diversity,
+                            biomass = log_ratio_bio,
+                            change = FALSE)
+  ),
+
+  # run sem for drivers, biomass (calculated) and diversity, only cutting
+  tar_target(
+    name = sem_cutting_change,
+    command = run_SEM(data = prep_cutting_change_data,
+                      landuse = "cutting")
+  ),
+
+  # summary
+  tar_target(
+    name = sem_cutting_change_result,
+    command = summary(sem_cutting_change)
+
+  ),
+
+  # sem cutting figure
+  tar_target(
+    name = sem_cutting_change_fig,
+    command = make_SEM_change_figure(sem_results = sem_cutting_change_result,
+                              landuse = "cutting")
+  ),
+
+
+  # SEM final biomass and diversity
+  # grazing
   tar_target(
     name = prep_grazing_data,
     command = prep_SEM_data(data = biomass_div,
                             landuse = "grazing",
-                            diversity = log_ratio_diversity)
+                            diversity = final_diversity,
+                            biomass = final_bio,
+                            change = FALSE)
   ),
 
   # run sem for drivers, biomass (calculated) and diversity, only grazing
@@ -61,9 +108,55 @@ piecewiseSEM_plan <- list(
                               landuse = "grazing")
   ),
 
+  # only direct effects
   tar_target(
-    name = sem_fig,
+    name = direct_sem_grazing,
+    command = run_direct_SEM(data = prep_grazing_data,
+                             landuse = "grazing",
+                             diversity = log_ratio_diversity,
+                             biomass = log_ratio_bio)
+  ),
+
+  # SEM change in biomass and diversity
+  # grazing
+  tar_target(
+    name = prep_grazing_change_data,
+    command = prep_SEM_data(data = biomass_div,
+                            landuse = "grazing",
+                            diversity = log_ratio_diversity,
+                            biomass = log_ratio_bio,
+                            change = FALSE)
+  ),
+
+  # run sem for drivers, biomass (calculated) and diversity, only cutting
+  tar_target(
+    name = sem_grazing_change,
+    command = run_SEM(data = prep_grazing_change_data,
+                      landuse = "grazing")
+  ),
+
+  # summary
+  tar_target(
+    name = sem_grazing_change_result,
+    command = summary(sem_grazing_change)
+
+  ),
+
+  # sem cutting figure
+  tar_target(
+    name = sem_grazing_change_fig,
+    command = make_SEM_change_figure(sem_results = sem_grazing_change_result,
+                              landuse = "grazing")
+  ),
+
+  tar_target(
+    name = sem_final_fig,
     command = sem_cutting_fig + sem_grazing_fig + plot_annotation(tag_levels = list(c("a) Cutting", "b) Grazing")))
+  ),
+
+  tar_target(
+    name = sem_change_fig,
+    command = sem_cutting_change_fig + sem_grazing_change_fig + plot_annotation(tag_levels = list(c("a) Cutting", "b) Grazing")))
   )
 
 )
