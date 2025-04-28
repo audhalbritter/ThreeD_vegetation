78a3b06e06fbe6d5bc2c31b22d8a49ee8c47acdd
audhalbritter <me@audhalbritter.com>
2025-04-28 16:32:41 +0200

    unclear changes

--- make.R
+++ make.R
@@ -3,7 +3,7 @@ source("other_scripts/load_libraries.R")
 
 # make the targets that are out of date
 # looks for a file called "_targets.R" in the working directory
-tar_make()
+tar_make(-manuscript, -si)
 tar_load_everything()
 # view pipeline and show which targets are out of date
 tar_visnetwork()
