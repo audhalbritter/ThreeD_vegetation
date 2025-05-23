0278a449d63449f48e89d2aa3b9e7c74db822b33
audhalbritter <me@audhalbritter.com>
2025-05-23 09:46:04 +0200

    add si back in

--- R/manuscript_plan.R
+++ R/manuscript_plan.R
@@ -6,8 +6,8 @@ manuscript_plan <- list(
   tar_quarto(name = ms,
     path = "manuscript/main_manuscript.qmd"),
 
-    # SI
-    tar_quarto(name = si,
-      path = "manuscript/SI.qmd")
+  # SI
+  tar_quarto(name = si,
+    path = "manuscript/SI.qmd")
 
 )
