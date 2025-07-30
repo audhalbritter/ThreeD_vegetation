69e2646a9de7da72d318a81816c07d4481a31e92
audhalbritter <me@audhalbritter.com>
2025-07-30 18:10:28 +0200

    Analysis_plan.R: remove across site model

--- R/analysis_plan.R
+++ R/analysis_plan.R
@@ -3,75 +3,9 @@
 analysis_plan <- list(
 
   ### STANDING BIOMASS AND DIVERSITY GLOBAL CHANGE MODEL
-  tar_target(
-    name = biomass_model_all,
-    command = {
+  
 
-      # test biomass in 2022
-      bio_model <- run_full_model_v2(dat = biomass_div |>
-                                       filter(grazing != "Natural"),
-                                     response = final_bio,
-                                     grazing_var = grazing_num) |>
-        # make long table
-        pivot_longer(cols = -c(data),
-                     names_sep = "_",
-                     names_to = c(".value", "names")) |>
-        unnest(glance) |>
-        select(data:adj.r.squared, AIC)
-    }
-  ),
-
-  # only interaction model
-  tar_target(
-    name = biomass_model,
-    command = biomass_model_all |>
-      # select parsimonious model
-      filter(AIC == min(AIC))
-  ),
-
-  # # check models
-  # tar_quarto(name = model_check,
-  #            path = "R/model_output.qmd"),
-
-
-  tar_target(
-    name = biomass_output,
-    command =  make_prediction_v2(biomass_model)
-
-  ),
-
-  # prepare model output
-  tar_target(
-    name =   biomass_prediction,
-    command = biomass_output |>
-      # merge data and prediction
-      mutate(output = map2(.x = newdata, .y = prediction, ~ bind_cols(.x, .y))) |>
-      select(output) |>
-      unnest(output) |>
-      rename(prediction = fit)
-  ),
-
-  # stats
-  tar_target(
-    name =   biomass_anova_table,
-    command = biomass_output |>
-      select(names, anova_tidy) |>
-      unnest(anova_tidy) |>
-      ungroup() |>
-      fancy_stats()
-  ),
-
-  # stats
-  tar_target(
-    name =   biomass_summary_table,
-    command = biomass_output |>
-      select(names, result) |>
-      unnest(result) |>
-      ungroup() |>
-      fancy_stats()
-  ),
-
-  ### biomass by origin
+  ### biomass separate by origin
 
   tar_target(
     name = biomass_model_all_origin,
@@ -144,100 +78,6 @@ analysis_plan <- list(
   # grazing intensity
   # run 3-way interaction model for cover
 
-  # run main effects model first
-  tar_target(
-    name = diversity_main_model,
-    command = {
-
-      run_full_model_v2(dat = biomass_div |>
-                       filter(grazing != "Natural"),
-                        response = final_diversity,
-                        grazing_var = grazing_num) |>
-        # make long table
-        pivot_longer(cols = -c(data),
-                     names_sep = "_",
-                     names_to = c(".value", "names")) |>
-        unnest(glance) |>
-        select(names:adj.r.squared, AIC)
-    }
-
-  ),
-
-  tar_target(
-    name = diversity_model_all,
-    command = {
-      dd <- biomass_div |>
-        pivot_longer(cols = -c(origSiteID:log_ratio_evenness, delta_bio:final_bio),
-                     names_to = "diversity_index",
-                     values_to = "value") |>
-          filter(grazing != "Natural")
-
-      run_full_model_v3(dat = dd,
-                     group = c("diversity_index"),
-                     response = value,
-                     grazing_var = grazing_num,
-                     biomass = final_bio) |>
-        # make long table
-        pivot_longer(cols = -c(diversity_index, data),
-                     names_sep = "_",
-                     names_to = c(".value", "mod", "names")) |>
-        unnest(glance) |>
-        select(diversity_index:adj.r.squared, AIC) |>
-        mutate(diversity_index = case_match(diversity_index,
-                                            "final_richness" ~ "richness",
-                                            "final_diversity" ~ "diversity",
-                                            "final_evenness" ~ "evenness"),
-               diversity_index = factor(diversity_index, levels = c("richness", "diversity", "evenness")))
-    }
-
-  ),
-
-  tar_target(
-    name = diversity_interaction_model,
-    command = diversity_model_all |>
-      group_by(diversity_index) |>
-      filter(mod == "interaction")
-  ),
-
-  tar_target(
-    name = diversity_output,
-    command = make_prediction_v2(diversity_interaction_model)
-
-  ),
-
-  # prepare model output
-  tar_target(
-    name = diversity_prediction,
-    command = diversity_output |>
-      # merge data and prediction
-      mutate(output = map2(.x = newdata, .y = prediction, ~ bind_cols(.x, .y))) |>
-      select(diversity_index, output) |>
-      unnest(output) |>
-      rename(prediction = fit) #|>
-      #mutate(diversity_index = factor(diversity_index, levels = c("richness", "diversity", "evenness")))
-  ),
-
-
-  # stats
-  tar_target(
-    name =   diversity_anova_table,
-    command = diversity_output |>
-      select(diversity_index, names, anova_tidy) |>
-      unnest(anova_tidy) |>
-      ungroup() |>
-      fancy_stats()
-  ),
-
-  tar_target(
-    name =   diversity_summary_table,
-    command = diversity_output |>
-      select(diversity_index, names, result) |>
-      unnest(result) |>
-      ungroup() |>
-      fancy_stats()
-  ),
-
-
   # analysis by origin
   tar_target(
     name = diversity_model_all_origin,
@@ -248,17 +88,29 @@ analysis_plan <- list(
                      values_to = "value") |>
         filter(grazing != "Natural")
 
-      run_full_model_v3_origin(dat = dd,
-                        group = c("origSiteID", "diversity_index"),
-                        response = value,
-                        grazing_var = grazing_num,
-                        biomass = final_bio) |>
+      all <- run_full_model(dat = dd,
+                            group = c("origSiteID", "diversity_index"),
+                            response = value,
+                            grazing_var = grazing_num) |>
         # make long table
         pivot_longer(cols = -c(origSiteID, diversity_index, data),
                      names_sep = "_",
                      names_to = c(".value", "mod", "names")) |>
         unnest(glance) |>
-        select(diversity_index:adj.r.squared, AIC) |>
+        filter(mod == "interaction") |>
+        select(origSiteID:adj.r.squared, AIC) |>
+
+      # run_full_model_v3_origin(dat = dd,
+      #                   group = c("origSiteID", "diversity_index"),
+      #                   response = value,
+      #                   grazing_var = grazing_num,
+      #                   biomass = final_bio) |>
+      #   # make long table
+      #   pivot_longer(cols = -c(origSiteID, diversity_index, data),
+      #                names_sep = "_",
+      #                names_to = c(".value", "mod", "names")) |>
+      #   unnest(glance) |>
+      #   select(diversity_index:adj.r.squared, AIC) |>
         mutate(diversity_index = case_match(diversity_index,
                                             "final_richness" ~ "richness",
                                             "final_diversity" ~ "diversity",
@@ -271,13 +123,16 @@ analysis_plan <- list(
   tar_target(
     name = diversity_origin_model,
     command = diversity_model_all_origin |>
-      group_by(diversity_index) |>
-      filter(mod == "interaction")
+      group_by(origSiteID,diversity_index) |>
+      #filter(mod == "interaction")
+      # select parsimonious model (by HAND!!!)
+      filter(names == "linear")
   ),
 
   tar_target(
     name = diversity_origin_output,
-    command = make_prediction_v2_origin(diversity_origin_model)
+    command = #make_prediction_v2_origin(diversity_origin_model)
+              make_prediction(diversity_origin_model)
 
   ),
 
@@ -289,17 +144,15 @@ analysis_plan <- list(
       mutate(output = map2(.x = newdata, .y = prediction, ~ bind_cols(.x, .y))) |>
       select(origSiteID, diversity_index, output) |>
       unnest(output) |>
-      rename(prediction = fit) #|>
-    #mutate(diversity_index = factor(diversity_index, levels = c("richness", "diversity", "evenness")))
+      rename(prediction = fit)
   ),
-
 
   # stats
   tar_target(
     name =   diversity_origin_anova_table,
     command = diversity_origin_output |>
       select(origSiteID, diversity_index, names, anova_tidy) |>
-      unnest(anova_tidy) |>
+      unnest(anova_tidy) |> 
       ungroup() |>
       fancy_stats()
   ),
