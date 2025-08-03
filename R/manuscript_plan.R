45130244c2301b58765d7fd0867ff5cb5c49c6ad
audhalbritter <me@audhalbritter.com>
2025-08-03 12:12:39 +0200

    Delete htmls

--- R/manuscript_plan.R
+++ R/manuscript_plan.R
@@ -10,4 +10,24 @@ manuscript_plan <- list(
   tar_quarto(name = si,
     path = "manuscript/SI.qmd")
 
+  # tar_target(
+  #   name = render_manuscript_pdf,
+  #   command = {
+  #     quarto::quarto_render(
+  #       input = "manuscript/main_manuscript.qmd",
+  #       output_format = "pdf"
+  #     )
+  #   }
+  # ),
+
+  # tar_target(
+  #   name = render_manuscript_docx,
+  #   command = {
+  #     quarto::quarto_render(
+  #       input = "manuscript/main_manuscript.qmd",
+  #       output_format = "docx"
+  #     )
+  #   }
+  # )
+
 )
