f522046143b0c197b944f11f82b22c5cd83c02fa
audhalbritter <me@audhalbritter.com>
2025-08-05 18:10:41 +0200

    Add SEM analyses for affinity traits across Alpine and Sub-alpine sites, including PC1 and PC2 evaluations, and generate combined figures.

--- R/piecewiseSEM_plan.R
+++ R/piecewiseSEM_plan.R
@@ -66,6 +66,100 @@ piecewiseSEM_plan <- list(
       dat <- biomass_div |> 
         filter(grazing != "Natural") |>
         tidylog::left_join(bind_rows(
+        affinity_pca[[1]]
+      ), by = join_by(origSiteID, warming, grazing, grazing_num, Nlevel, Namount_kg_ha_y, Nitrogen_log))
+
+      # prep data
+      dat1 <- prep_SEM_data(data = dat,
+                            landuse = "clipping",
+                            diversity = PC1,
+                            biomass = final_bio)
+
+      # affinity alpine
+      mod1 <- run_SEM(data = dat1 |>
+                        filter(origSiteID == "Alpine"),
+                      landuse = "clipping")
+
+      out1 <- summary(mod1)
+
+      fig1 <- make_SEM_figure(sem_results = out1,
+                              type = "final",
+                              landuse = "clipping",
+                              col = treatment_palette,
+                              diversity_type = "affinity")
+
+      # affinity sub-alpine
+      mod2 <- run_SEM(data = dat1 |>
+                        filter(origSiteID == "Sub-alpine"),
+                      landuse = "clipping")
+
+      out2 <- summary(mod2)
+
+      fig2 <- make_SEM_figure(sem_results = out2,
+                              type = "final",
+                              landuse = "clipping",
+                              col = treatment_palette,
+                              diversity_type = "affinity")
+
+      # PC2 analyses
+      # prep data for PC2
+      dat2 <- prep_SEM_data(data = dat,
+                            landuse = "clipping",
+                            diversity = PC2,
+                            biomass = final_bio)
+
+      # PC2 affinity alpine
+      mod3 <- run_SEM(data = dat2 |>
+                        filter(origSiteID == "Alpine"),
+                      landuse = "clipping")
+
+      out3 <- summary(mod3)
+
+      fig3 <- make_SEM_figure(sem_results = out3,
+                              type = "final",
+                              landuse = "clipping",
+                              col = treatment_palette,
+                              diversity_type = "affinity")
+
+      # PC2 affinity sub-alpine
+      mod4 <- run_SEM(data = dat2 |>
+                        filter(origSiteID == "Sub-alpine"),
+                      landuse = "clipping")
+
+      out4 <- summary(mod4)
+
+      fig4 <- make_SEM_figure(sem_results = out4,
+                              type = "final",
+                              landuse = "clipping",
+                              col = treatment_palette,
+                              diversity_type = "affinity")
+
+      figure <- (fig1 + fig2) / (fig3 + fig4) +
+        plot_annotation(tag_levels = list(c('a) Alpine PC1', 'b) Sub-alpine PC1', 'c) Alpine PC2', 'd) Sub-alpine PC2'))) &
+        theme(plot.tag.position = c(0, 1),
+              plot.tag = element_text(size = 12, hjust = 0, vjust = 0))
+
+      out <- bind_rows(
+        "Affinity: Alpine PC1" = out1$coefficients,
+        "Affinity: Sub-alpine PC1" = out2$coefficients,
+        "Affinity: Alpine PC2" = out3$coefficients,
+        "Affinity: Sub-alpine PC2" = out4$coefficients,
+        .id = "Type"
+      )
+
+      outputList <- list(figure, out)
+
+    }
+  ),
+
+  tar_target(
+    name = cut_affinity2,
+    command = {
+
+      # affinity data
+      dat <- biomass_div |> 
+        filter(grazing != "Natural") |>
+        tidylog::left_join(bind_rows(
         affinity_alpine_pca[[1]],
         affinity_subalpine_pca[[1]]
       ), by = join_by(origSiteID, warming, grazing, grazing_num, Nlevel, Namount_kg_ha_y, Nitrogen_log))
