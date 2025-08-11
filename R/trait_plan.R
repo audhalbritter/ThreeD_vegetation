fed1d5b4b6dc2d665195034681c3f05c123cb275
audhalbritter <me@audhalbritter.com>
2025-08-11 10:06:10 +0200

    Remove trait PCA targets from piecewiseSEM_plan.R and trait_plan.R

--- R/trait_plan.R
+++ R/trait_plan.R
@@ -26,14 +26,6 @@ trait_plan <- list(
     )
   ),
 
-      tar_target(
-    name = trait_pca,
-    command = make_trait_pca(trait_mean |> 
-                            filter(grazing != "Natural") |>
-                            filter(trait_trans %in% c("plant_height_cm_log", "dry_mass_g_log", "leaf_area_cm2_log", "leaf_thickness_mm_log", "sla_cm2_g", "ldmc"))
-    )
-  ),
-
   tar_target(
     name = affinity_alpine_pca,
     command = make_trait_pca(trait_mean |> 
@@ -53,48 +45,22 @@ trait_plan <- list(
   ),
 
   tar_target(
-    name = trait_alpine_pca,
-    command = make_trait_pca(trait_mean |> 
-                            filter(grazing != "Natural") |>
-                            filter(origSiteID == "Alpine") |>
-                            filter(trait_trans %in% c("plant_height_cm_log", "dry_mass_g_log", "leaf_area_cm2_log", "leaf_thickness_mm_log", "sla_cm2_g", "ldmc"))
-    )
-  ),
-
-  tar_target(
-    name = trait_subalpine_pca,
-    command = make_trait_pca(trait_mean |> 
-                            filter(grazing != "Natural") |>
-                            filter(origSiteID == "Sub-alpine") |>
-                            filter(trait_trans %in% c("plant_height_cm_log", "dry_mass_g_log", "leaf_area_cm2_log", "leaf_thickness_mm_log", "sla_cm2_g", "ldmc"))
-    )
-  ),
-
-  tar_target(
     name = affinity_pca_plot,
     command = make_pca_plot_sites(affinity_pca, title = "Affinity", color_warm = warming_palette)
-  ),
-    tar_target(
-    name = trait_pca_plot,
-    command = make_pca_plot_sites(trait_pca, title = "Trait", color_warm = warming_palette)
   ),
 
   # make trait pca plots
   tar_target(
-    name = trait_pca_plot_single,
+    name = affinity_pca_plot_single,
     command = {
       # Create individual plots with tags
               p1 <- make_pca_plot(affinity_alpine_pca, title = "Affinity: alpine", color_warm = warming_palette) + 
         labs(tag = "a)")
               p2 <- make_pca_plot(affinity_subalpine_pca, title = "Affinity: sub-alpine", color_warm = warming_palette) + 
         labs(tag = "b)")
-              p3 <- make_pca_plot(trait_alpine_pca, title = "Traits: alpine", color_warm = warming_palette) + 
-        labs(tag = "c)")
-              p4 <- make_pca_plot(trait_subalpine_pca, title = "Traits: sub-alpine", color_warm = warming_palette) + 
-        labs(tag = "d)")
       
       # Combine with patchwork
-      (p1 + p2) / (p3 + p4) +
+      (p1 + p2) +
         plot_layout(guides = "collect") &
         theme(legend.position = "bottom")
     }
