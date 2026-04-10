7cdec176039074acfcca8afefa90de4070ac5b55
audhalbritter <me@audhalbritter.com>
2026-04-10 16:51:20 +0200

    update discussion from comments

--- make.R
+++ make.R
@@ -4,6 +4,7 @@ source("other_scripts/load_libraries.R")
 # make the targets that are out of date
 # looks for a file called "_targets.R" in the working directory
 targets::tar_make()
+
 # tar_make(-manuscript, -si)
 tar_load_everything()
 # view pipeline and show which targets are out of date
@@ -25,4 +26,4 @@ tar_visnetwork()
 ggsave("output/Fig3_biomass_div.png", bio_div_figure, dpi = 300, height = 6, width = 7)
 ggsave("output/Fig4_sem_fig.png", cut_final_diversity[[1]], dpi = 300, height = 4, width = 10)
 ggsave("output/Fig5_standingB_div.png", standingB_div_final_figure, dpi = 300, height = 5, width = 8)
-#ggsave("output/Fig6_toenail.png", , dpi = 300, height = 12, width = 8)
+# ggsave("output/Fig6_toenail.png", , dpi = 300, height = 12, width = 8)
